0e908a1c58052f993b0075c72ae254d3c82f20d7
Kenneth Velásquez <kvelasquez-2019504@kinal.edu.gt>
2025-01-08 23:36:05 -0600

    refactor: import of maintenance components to LayoutMaintenance

--- src/modules/maintenance/components/Header.vue
+++ src/modules/maintenance/components/Header.vue
@@ -1,20 +1,12 @@
-<script>
-export default {
-  props: {
-    name: {
-      type: String,
-      default: 'Header',
-    },
-  },
-};
-</script>
-
 <template>
-  <h1>This is: {{ name }}!</h1>
+  <section class="flex items-center justify-center text-xl">
+    <h1 class="top-0 font-extrabold text-5xl mt-2 mb-2 text-white md:h1-xl">tailmatee</h1>
+  </section>
 </template>
 
 <style scoped>
-h1 {
-  color: blue;
-}
+  section {
+    font: Montserrat;
+    background-color: #11113A;
+  }
 </style>
