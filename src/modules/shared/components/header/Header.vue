3a67a60e4e9906495df1339148dddd14da647fa4
Abr10chino <jorgechino866@gmail.com>
2025-01-14 01:13:26 -0600

    refactor(Header): Change design header in mobil and desktop

--- src/modules/shared/components/header/Header.vue
+++ src/modules/shared/components/header/Header.vue
@@ -1,62 +1,60 @@
 <template>
-  <header class="bg-white shadow-sm fixed w-full top-0 z-50">
-    <nav class="container mx-auto px-4 py-4">
-      <div class="flex items-center justify-between">
-        <a href="/">
-          <img
-          src="/src/assets/icons/logotipo-positivo.svg"
-          alt="TailMatee Logo"
-          class="h-10 md:h-10 object-contain"
-          />
-        </a>
-        <div class="hidden md:flex space-x-8">
-          <a
-            v-for="item in menuItems"
-            :key="item.id"
-            :href="item.href"
-            class="text-gray-600 hover:text-gray-900 transition-all duration-300 py-2 relative group"
-            :aria-label="`Navigate to ${item.label}`"
-          >
-            {{ item.label }}
-            <span
-              class="absolute bottom-0 left-0 w-full h-0.5 bg-gray-900 transform scale-x-0 transition-transform duration-300 group-hover:scale-x-100"
-            ></span>
-          </a>
-        </div>
+  <nav class="bg-[#11113A]">
+    <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
 
-        <button
-          class="md:hidden p-2 rounded-lg hover:bg-gray-100 transition-colors duration-300"
-          @click="toggleMenu"
-          aria-label="Toggle navigation menu"
+      <a href="/" class="flex items-left justify-left space-x-3 rtl:space-x-reverse">
+        <span class="self-center text-5xl font-bold whitespace-nowrap dark:text-white font-[Montserrat] text-left tracking-[-5px]">tailmatee</span>
+      </a>
+
+      <a
+        href="#"
+        @click.prevent="toggleMenu"
+        class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-[#11113A] focus:outline-none focus:ring-2 dark:text-gray-400"
+        aria-controls="navbar-default"
+        :aria-expanded="isOpen"
+      >
+        <svg
+          class="w-5 h-5"
+          aria-hidden="true"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 17 14"
         >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            class="h-6 w-6 text-gray-600"
-            fill="none"
-            viewBox="0 0 24 24"
+          <path
             stroke="currentColor"
+            stroke-linecap="round"
+            stroke-linejoin="round"
             stroke-width="2"
-          >
-            <path stroke-linecap="round" stroke-linejoin="round" d="M4 6h16M4 12h16M4 18h16" />
-          </svg>
-        </button>
-      </div>
+            d="M1 1h15M1 7h15M1 13h15"
+          />
+        </svg>
+      </a>
 
-      <div v-if="isOpen" class="md:hidden">
-        <div class="px-2 pt-2 pb-3 space-y-1">
-          <a
-            v-for="item in menuItems"
-            :key="item.id"
-            :href="item.href"
-            class="block px-3 py-2 text-gray-600 hover:text-gray-900 hover:bg-gray-50 rounded-md transition-colors duration-300"
-            :aria-label="`Navigate to ${item.label}`"
+      <div :class="['w-full md:block md:w-auto', { hidden: !isOpen }]">
+        <ul
+          :class="[ 
+            'font-[Montserrat] text-lg flex flex-col p-4 md:p-0 mt-4 border border-transparent rounded-lg md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 dark:border-transparent',
+            isOpen ? 'bg-[#11113A]' : 'bg-transparent',
+            'items-center'
+          ]"
+        >
+          <li 
+            v-for="(item, index) in menuItems" 
+            :key="item.id" 
+            class="flex justify-center w-full border-b-2 border-gray-600 md:border-transparent"
+            :class="{ 'border-b-0': index === menuItems.length - 1 }"
           >
-            {{ item.label }}
-          </a>
-        </div>
+            <a
+              :href="item.href"
+              class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+            >
+              {{ item.label }}
+            </a>
+          </li>
+        </ul>
       </div>
-    </nav>
-  </header>
+    </div>
+  </nav>
 </template>
 
 <script>
