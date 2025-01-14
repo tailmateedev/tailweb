7a73f033ec2b6c956902708641281369e10df225
Abr10chino <jorgechino866@gmail.com>
2025-01-14 16:18:02 -0600

    refactor(Footer): Change footer style

--- src/modules/shared/components/footer/Footer.vue
+++ src/modules/shared/components/footer/Footer.vue
@@ -1,101 +1,66 @@
 <template>
-  <footer class="bg-white">
-    <div class="mx-auto max-w-screen-xl space-y-4 px-4 py-6 sm:px-6 lg:space-y-16 lg:px-8">
-      <div class="grid grid-cols-2 gap-8 lg:grid-cols-2">
-        <div>
-          <div class="text-teal-600">
-            <img src="/src/icons/horizontal-positivo.svg" alt="tailmatee-logo" width="220px" height="100px">
-
-          </div>
-          <ul class="mt-8 flex gap-6">
-            <li>
-              <a href="#" rel="noreferrer" target="_blank" class="text-gray-700 transition hover:opacity-75">
-                <span class="sr-only">Facebook</span>
-                <svg class="size-6" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
-                  <path fill-rule="evenodd"
-                    d="M22 12c0-5.523-4.477-10-10-10S2 6.477 2 12c0 4.991 3.657 9.128 8.438 9.878v-6.987h-2.54V12h2.54V9.797c0-2.506 1.492-3.89 3.777-3.89 1.094 0 2.238.195 2.238.195v2.46h-1.26c-1.243 0-1.63.771-1.63 1.562V12h2.773l-.443 2.89h-2.33v6.988C18.343 21.128 22 16.991 22 12z"
-                    clip-rule="evenodd" />
-                </svg>
-              </a>
-            </li>
-            <li>
-              <a href="#" rel="noreferrer" target="_blank" class="text-gray-700 transition hover:opacity-75">
-                <span class="sr-only">Instagram</span>
-
-                <svg class="size-6" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
-                  <path fill-rule="evenodd"
-                    d="M12.315 2c2.43 0 2.784.013 3.808.06 1.064.049 1.791.218 2.427.465a4.902 4.902 0 011.772 1.153 4.902 4.902 0 011.153 1.772c.247.636.416 1.363.465 2.427.048 1.067.06 1.407.06 4.123v.08c0 2.643-.012 2.987-.06 4.043-.049 1.064-.218 1.791-.465 2.427a4.902 4.902 0 01-1.153 1.772 4.902 4.902 0 01-1.772 1.153c-.636.247-1.363.416-2.427.465-1.067.048-1.407.06-4.123.06h-.08c-2.643 0-2.987-.012-4.043-.06-1.064-.049-1.791-.218-2.427-.465a4.902 4.902 0 01-1.772-1.153 4.902 4.902 0 01-1.153-1.772c-.247-.636-.416-1.363-.465-2.427-.047-1.024-.06-1.379-.06-3.808v-.63c0-2.43.013-2.784.06-3.808.049-1.064.218-1.791.465-2.427a4.902 4.902 0 011.153-1.772A4.902 4.902 0 015.45 2.525c.636-.247 1.363-.416 2.427-.465C8.901 2.013 9.256 2 11.685 2h.63zm-.081 1.802h-.468c-2.456 0-2.784.011-3.807.058-.975.045-1.504.207-1.857.344-.467.182-.8.398-1.15.748-.35.35-.566.683-.748 1.15-.137.353-.3.882-.344 1.857-.047 1.023-.058 1.351-.058 3.807v.468c0 2.456.011 2.784.058 3.807.045.975.207 1.504.344 1.857.182.466.399.8.748 1.15.35.35.683.566 1.15.748.353.137.882.3 1.857.344 1.054.048 1.37.058 4.041.058h.08c2.597 0 2.917-.01 3.96-.058.976-.045 1.505-.207 1.858-.344.466-.182.8-.398 1.15-.748.35-.35.566-.683.748-1.15.137-.353.3-.882.344-1.857.048-1.055.058-1.37.058-4.041v-.08c0-2.597-.01-2.917-.058-3.96-.045-.976-.207-1.505-.344-1.858a3.097 3.097 0 00-.748-1.15 3.098 3.098 0 00-1.15-.748c-.353-.137-.882-.3-1.857-.344-1.023-.047-1.351-.058-3.807-.058zM12 6.865a5.135 5.135 0 110 10.27 5.135 5.135 0 010-10.27zm0 1.802a3.333 3.333 0 100 6.666 3.333 3.333 0 000-6.666zm5.338-3.205a1.2 1.2 0 110 2.4 1.2 1.2 0 010-2.4z"
-                    clip-rule="evenodd" />
-                </svg>
-              </a>
-            </li>
-            <li>
-              <a href="https://www.linkedin.com" rel="noreferrer" target="_blank"
-                class="text-gray-700 transition hover:opacity-75">
-                <span class="sr-only">LinkedIn</span>
-                <svg class="size-6" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
-                  <path
-                    d="M20.452 3H3.548C3.24 3 3 3.237 3 3.533v16.934c0 .296.24.533.548.533h16.904c.308 0 .548-.237.548-.533V3.533c0-.296-.24-.533-.548-.533zM8.355 18.356H5.912v-8.88h2.443v8.88zM7.133 8.647a1.418 1.418 0 110-2.835 1.418 1.418 0 010 2.835zm11.222 9.71h-2.444v-4.712c0-1.122-.023-2.563-1.563-2.563-1.565 0-1.805 1.222-1.805 2.485v4.79h-2.443v-8.88h2.344v1.213h.032c.327-.622 1.125-1.278 2.317-1.278 2.478 0 2.936 1.63 2.936 3.748v5.197z" />
-                </svg>
-              </a>
-            </li>
-            <li>
-              <a href="https://www.twitter.com" rel="noreferrer" target="_blank"
-                class="text-gray-700 transition hover:opacity-75">
-                <span class="sr-only">X (Twitter)</span>
-                <svg class="size-6" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
-                  <path
-                    d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84" />
-                </svg>
-              </a>
-            </li>
-          </ul>
-        </div>
-
-        <div>
-          <div>
-            <p class="font-medium text-gray-900">Tailmatee</p>
-            <ul class="mt-6 space-y-4 text-sm">
-              <li>
-                <a href="#" class="text-gray-700 transition hover:opacity-75" @click.prevent="navigateToHome">Inicio</a>
-              </li>
-              <li>
-                <a href="/contact" class="text-gray-700 transition hover:opacity-75">Contactanos</a>
-              </li>
-              <li>
-                <a href="/business" class="text-gray-700 transition hover:opacity-75"
-                  @click.prevent="navigateToBusiness">Tailmatee Business</a>
-              </li>
-            </ul>
-          </div>
-        </div>
+  <footer class="bg-zinc-50 dark:bg-[#11113A] text-surface dark:text-white text-center flex flex-col items-center">
+    <div class="container pt-6 pb-2">
+      <div class="flex justify-center space-x-3">
+        <a
+          v-for="icon in socialMediaIcons"
+          :key="icon.id"
+          :href="icon.link"
+          :aria-label="icon.name"
+          target="_blank"
+          rel="noopener noreferrer"
+          class="flex items-center justify-center w-10 h-5 rounded-full bg-transparent text-current transition"
+        >
+          <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 512 512" class="w-6 h-6">
+            <path :d="icon.path" />
+          </svg>
+        </a>
       </div>
-
-      <p class="text-xs text-gray-500 flex justify-center">&copy; 2024. Company Name. All rights reserved.</p>
+    </div>
+    <div class="bg-transparent p-4 text-center text-surface dark:text-white font-[Montserrat] font-bold">
+      © 2025 Copyright: tailmatee
     </div>
   </footer>
 </template>
 
-<script setup>
-function navigateToHome(event) {
-  event.preventDefault();
-  const currentPath = window.location.pathname;
-
-  if (currentPath === "/") {
-    window.scrollTo({ top: 0, behavior: "smooth" });
-  } else {
-    window.location.href = "/";
-  }
-}
-
-function navigateToBusiness(event) {
-  event.preventDefault();
-  const currentPath = window.location.pathname;
-
-  if (currentPath === "/business") {
-    window.scrollTo({ top: 0, behavior: "smooth" });
-  } else {
-    window.location.href = "/business";
-  }
-}
+<script>
+export default {
+  name: "Footer",
+  data() {
+    return {
+      socialMediaIcons: [
+        {
+          id: 1,
+          name: "Facebook",
+          link: "https://www.facebook.com/tailmatee",
+          path: "M80 299.3V512H196V299.3h86.5l18-97.8H196V166.9c0-51.7 20.3-71.5 72.7-71.5c16.3 0 29.4 .4 37 1.2V7.9C291.4 4 256.4 0 236.2 0C129.3 0 80 50.5 80 159.4v42.1H14v97.8H80z",
+        },
+        {
+          id: 2,
+          name: "Twitter",
+          link: "https://x.com/tailmatee",
+          path: "M389.2 48h70.6L305.6 224.2 487 464H345L233.7 318.6 106.5 464H35.8L200.7 275.5 26.8 48H172.4L272.9 180.9 389.2 48zM364.4 421.8h39.1L151.1 88h-42L364.4 421.8z",
+        },
+        {
+          id: 3,
+          name: "Instagram",
+          link: "https://www.instagram.com/tailmatee",
+          path: "M224.1 141c-63.6 0-114.9 51.3-114.9 114.9s51.3 114.9 114.9 114.9S339 319.5 339 255.9 287.7 141 224.1 141zm0 189.6c-41.1 0-74.7-33.5-74.7-74.7s33.5-74.7 74.7-74.7 74.7 33.5 74.7 74.7-33.6 74.7-74.7 74.7zm146.4-194.3c0 14.9-12 26.8-26.8 26.8-14.9 0-26.8-12-26.8-26.8s12-26.8 26.8-26.8 26.8 12 26.8 26.8zm76.1 27.2c-1.7-35.9-9.9-67.7-36.2-93.9-26.2-26.2-58-34.4-93.9-36.2-37-2.1-147.9-2.1-184.9 0-35.8 1.7-67.6 9.9-93.9 36.1s-34.4 58-36.2 93.9c-2.1 37-2.1 147.9 0 184.9 1.7 35.9 9.9 67.7 36.2 93.9s58 34.4 93.9 36.2c37 2.1 147.9 2.1 184.9 0 35.9-1.7 67.7-9.9 93.9-36.2 26.2-26.2 34.4-58 36.2-93.9 2.1-37 2.1-147.8 0-184.8zM398.8 388c-7.8 19.6-22.9 34.7-42.6 42.6-29.5 11.7-99.5 9-132.1 9s-102.7 2.6-132.1-9c-19.6-7.8-34.7-22.9-42.6-42.6-11.7-29.5-9-99.5-9-132.1s-2.6-102.7 9-132.1c7.8-19.6 22.9-34.7 42.6-42.6 29.5-11.7 99.5-9 132.1-9s102.7-2.6 132.1 9c19.6 7.8 34.7 22.9 42.6 42.6 11.7 29.5 9 99.5 9 132.1s2.7 102.7-9 132.1z"
+        },
+        {
+          id: 4,
+          name: "Linkedin",
+          link: "https://www.linkedin.com/company/tailmatee/",
+          path: "M100.3 448H7.4V148.9h92.9zM53.8 108.1C24.1 108.1 0 83.5 0 53.8a53.8 53.8 0 0 1 107.6 0c0 29.7-24.1 54.3-53.8 54.3zM447.9 448h-92.7V302.4c0-34.7-.7-79.2-48.3-79.2-48.3 0-55.7 37.7-55.7 76.7V448h-92.8V148.9h89.1v40.8h1.3c12.4-23.5 42.7-48.3 87.9-48.3 94 0 111.3 61.9 111.3 142.3V448z"
+        },
+        {
+          id: 5,
+          name: "Tiktok",
+          link: "https://www.tiktok.com/@tailmatee",
+          path: "M412.19,118.66a109.27,109.27,0,0,1-9.45-5.5,132.87,132.87,0,0,1-24.27-20.62c-18.1-20.71-24.86-41.72-27.35-56.43h.1C349.14,23.9,350,16,350.13,16H267.69V334.78c0,4.28,0,8.51-.18,12.69,0,.52-.05,1-.08,1.56,0,.23,0,.47-.05.71,0,.06,0,.12,0,.18a70,70,0,0,1-35.22,55.56,68.8,68.8,0,0,1-34.11,9c-38.41,0-69.54-31.32-69.54-70s31.13-70,69.54-70a68.9,68.9,0,0,1,21.41,3.39l.1-83.94a153.14,153.14,0,0,0-118,34.52,161.79,161.79,0,0,0-35.3,43.53c-3.48,6-16.61,30.11-18.2,69.24-1,22.21,5.67,45.22,8.85,54.73v.2c2,5.6,9.75,24.71,22.38,40.82A167.53,167.53,0,0,0,115,470.66v-.2l.2.2C155.11,497.78,199.36,496,199.36,496c7.66-.31,33.32,0,62.46-13.81,32.32-15.31,50.72-38.12,50.72-38.12a158.46,158.46,0,0,0,27.64-45.93c7.46-19.61,9.95-43.13,9.95-52.53V176.49c1,.6,14.32,9.41,14.32,9.41s19.19,12.3,49.13,20.31c21.48,5.7,50.42,6.9,50.42,6.9V131.27C453.86,132.37,433.27,129.17,412.19,118.66Z"
+        },
+      ],
+    };
+  },
+};
 </script>
