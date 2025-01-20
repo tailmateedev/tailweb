e1271af2ee0806955cbea22f883d59736e98c15d
David <davidsandoval0051@gmail.com>
2025-01-20 14:08:57 -0600

    [feature]: maintenance

---
+++ modules/maintenance/components/CircleMedia.vue
@@ -0,0 +1,52 @@
+<script>
+export default {
+
+  props: {
+    url: {
+      type: String,
+      required: true,
+    },
+    icon: {
+      type: String,
+      required: true,
+    },
+    open: {
+      type: Boolean,
+      default: false
+    }
+  },
+};
+
+</script>
+
+<template>
+  <div v-if="open == true">
+    <a
+      :href="url"
+      target="_blank"
+      rel="noopener noreferrer"
+      class="rounded-full bg-[#11113A] text-white text-lg flex items-center justify-center hover:bg-[#0f0f2a] focus:outline-none p-1 mx-2"
+    >
+      <span v-html="icon" class="text-center"></span>
+    </a>
+  </div>
+  <div v-else>
+    <a
+      :href="url"
+      class="rounded-full bg-[#11113A] text-white text-lg flex items-center justify-center hover:bg-[#0f0f2a] focus:outline-none p-1 mx-2"
+    >
+      <span v-html="icon" class="text-center"></span>
+    </a>
+  </div>
+</template>
+
+<style>
+  .icon {
+    fill: white;
+    transition: fill 0.3s ease;
+  }
+
+  .icon-container:hover .icon {
+    fill: #808080;
+  }
+</style>
